04f4af8d3e24e53e9955b46ae49968412b8887b0
daisukelab <niz.edge@gmail.com>
2022-11-07 18:50:33 +0900

    Add AR_BYOLAX, a BYOL-A variant extended to stack features from all the layers.

--- evar/ar_byola.py
+++ evar/ar_byola.py
@@ -11,7 +11,7 @@ from evar.ar_base import (BaseAudioRepr, ToLogMelSpec, calculate_norm_stats, nor
 from evar.model_utils import load_pretrained_weights
 import logging
 try:
-    from external.byol_a.byol_a.models import AudioNTT2020Task6
+    from external.byol_a.byol_a.models import AudioNTT2020Task6, AudioNTT2020Task6X
 except Exception as e:
     logging.info(f'Make your copy of BYOL-A under external folder. Check Preparing-models.md for the details.')
 
@@ -33,6 +33,34 @@ class AR_BYOLA(BaseAudioRepr):
         x = normalize_spectrogram(self.norm_stats, x) # B,F,T
         x = x.unsqueeze(1)    # -> B,1,F,T
         x = self.body(x)      # -> B,T,D=C*F
+        x = x.transpose(1, 2) # -> B,D,T
+        return x
+
+    def forward(self, batch_audio):
+        x = self.encode_frames(batch_audio)
+        x = temporal_pooling(self, x)
+        return x
+
+
+class AR_BYOLAX(BaseAudioRepr):
+    """A BYOL-A variant extended to stack features from all the layers."""
+    def __init__(self, cfg):
+        super().__init__(cfg=cfg)
+        self.to_feature = ToLogMelSpec(cfg)
+
+        self.body = AudioNTT2020Task6X(n_mels=cfg.n_mels, d=cfg.feature_d)
+        if cfg.weight_file is not None:
+            self.body.load_weight(cfg.weight_file, device='cpu')
+        self.cfg.feature_d = self.cfg.feature_d * self.body.n_feature_layer
+
+    def precompute(self, device, data_loader):
+        self.norm_stats = calculate_norm_stats(device, data_loader, self.to_feature)
+
+    def encode_frames(self, batch_audio):
+        x = self.to_feature(batch_audio)
+        x = normalize_spectrogram(self.norm_stats, x) # B,F,T
+        x = x.unsqueeze(1)    # -> B,1,F,T
+        x = self.body(x, layered=True) # -> B,T,D=C*F*Layer
         x = x.transpose(1, 2) # -> B,D,T
         return x
 
